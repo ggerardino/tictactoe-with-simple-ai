402ac35f0a34b0285a4d4ba0129ddc443219490a
Gabriel Angelo Magsombol Gerardino <81841692+ggerardino@users.noreply.github.com>
2022-07-10 20:58:46 +0800

    Add files via upload

--- ai.py
+++ ai.py
@@ -2,24 +2,24 @@ import board
 import controller
 import math
 
-def ai_move():
+def ai_move(turn):
     bestScore = -math.inf
     bestMove = [0,0]
     for i in range(3):
         for j in range(3):
             if(board.getPiece(i, j) == " "):
                 board.updateBoard("X", i, j)
-                score = minimax(board, 0, False)
+                score = minimax(turn, board, 0, False)
                 board.updateBoard(" ", i, j)
                 if(score > bestScore):
                     bestScore = score
                     bestMove = [i, j]
     board.updateBoard("X", bestMove[0], bestMove[1])
 
-def minimax(board, depth, isMaximizing):
-    if(controller.check_winner() == 1):
+def minimax(turn, board, depth, isMaximizing):
+    if(controller.check_winner(turn) == 1):
         return 1
-    elif(controller.check_winner() == 0):
+    elif(controller.check_winner(turn) == 0):
         return -1
     elif(controller.check_draw() == False):
         return 0
@@ -30,7 +30,7 @@ def minimax(board, depth, isMaximizing):
             for j in range(3):
                 if(board.getPiece(i, j) == " "):
                     board.updateBoard("X", i, j)
-                    score = minimax(board, depth + 1, False)
+                    score = minimax(turn, board, depth + 1, False)
                     board.updateBoard(" ", i, j)
                     if(score > bestScore):
                         bestScore = score
@@ -41,7 +41,7 @@ def minimax(board, depth, isMaximizing):
             for j in range(3):
                 if(board.getPiece(i, j) == " "):
                     board.updateBoard("O", i, j)
-                    score = minimax(board, depth + 1, True)
+                    score = minimax(turn, board, depth + 1, True)
                     board.updateBoard(" ", i, j)
                     if(score < bestScore):
                         bestScore = score
